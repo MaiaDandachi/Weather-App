4006980d8b7a82f69d9927be215a999d10e5017a
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2021-06-05 11:59:48 +0300

    Fixed footer style

--- src/Components/Footer/FooterStyles.js
+++ src/Components/Footer/FooterStyles.js
@@ -2,9 +2,11 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   footer: {
-    marginTop: 'calc(5% + 40px)',
+    // marginTop: 'calc(5% + 40px)',
     bottom: 0,
     textAlign: 'center',
+    paddingTop: '30vh',
+    position: 'relative',
   },
 });
 
