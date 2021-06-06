506fc98577dc8f7c72ae5482fc84675b2cafcd41
Maia-Dandachi <Lynn.dandachi@bilgiedu.net>
2021-06-06 13:07:30 +0300

    Add custom .env varaible

--- src/actions/WeatherActions.js
+++ src/actions/WeatherActions.js
@@ -4,6 +4,9 @@ import {
   WEATHER_LIST_SUCCESS,
   WEATHER_LIST_FAIL,
 } from '../constants/WeatherConstants';
+// const { REACT_APP_API_ID } = process.env;
+
+// console.log(process.env);
 
 export const listWeather =
   (units = 'imperial') =>
@@ -13,7 +16,7 @@ export const listWeather =
 
       // Make request to OpenWeatherMap API
       const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=156c22169c72ad38806ee258dcf2fe3e&cnt=40&units=${units}`
+        `https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=${process.env.REACT_APP_API_ID}&cnt=40&units=${units}`
       );
 
       dispatch({ type: WEATHER_LIST_SUCCESS, payload: data });
